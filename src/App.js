9ac9e85475a538c0adc710a2175b08af110c77cf
Ved@vy@s7 <rvn582@gmail.com>
2019-09-10 00:08:26 +0530

    inital

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import apiDetails from './configs/apiConfigs';
+import Home from './components/home';
+function App() {
+  let [token, setToken] = useState('');
+  useEffect(() => {
+    axios
+      .post(
+        apiDetails.getToken.url,
+        {},
+        { headers: apiDetails.getToken.headers }
+      )
+      .then(res => {
+        setToken(res.data);
+      })
+      .catch(err => console.log(err));
+  }, []);
+  return <Home token={token} />;
+}
+
+export default App;
